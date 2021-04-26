692cef43dfe4f26cdf71ee6d5a6f5945c8f5163c
LeandroParisi <lepcbelisario@gmail.com>
2021-04-26 09:38:46 -0300

    feat: adds generalLinks file

---
+++ generalLinks.js
@@ -0,0 +1,10 @@
+const links = {
+  databaseBasicDotnet: 'https://trailhead.salesforce.com/pt-BR/content/learn/modules/database_basics_dotnet',
+  dbWorkbench: 'https://workbench.developerforce.com/login.php',
+},
+
+const extensions = [
+  'SalesForce inspector',
+  'SalesForce login helper',
+  
+]
