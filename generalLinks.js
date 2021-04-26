65f7a5e3553df2c56b11a1b467eabf5d38e95d51
LeandroParisi <lepcbelisario@gmail.com>
2021-04-26 09:48:53 -0300

    feat: adds mainLinks to general links file

--- generalLinks.js
+++ generalLinks.js
@@ -1,10 +1,16 @@
+const mainLinks = {
+  trailHead: 'https://trailhead.salesforce.com/pt-BR/home',
+  trailVivo: 'https://trailhead.salesforce.com/pt-BR/users/juliolima/trailmixes/vivo-developer',
+}
+
 const links = {
   databaseBasicDotnet: 'https://trailhead.salesforce.com/pt-BR/content/learn/modules/database_basics_dotnet',
   dbWorkbench: 'https://workbench.developerforce.com/login.php',
+  blockChain: 'https://trailhead.salesforce.com/pt-BR/content/learn/modules/blockchain-basics',
 },
 
 const extensions = [
   'SalesForce inspector',
   'SalesForce login helper',
-  
+
 ]
